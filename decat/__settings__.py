e3706a7f99f2a126d1038be149a91cb0514756c1
sudoMode <singh.mandeep2207@gmail.com>
2021-09-10 18:21:25 +0530

    added vocabulary files to build

--- decat/__settings__.py
+++ decat/__settings__.py
@@ -3,18 +3,20 @@ from os.path import join
 from pathlib import Path
 
 
-__project__ = 'decat'
-__description__ = """De-concatenate strings without white-spaces."""
-__author__ = 'Mandeep Singh'
+# version
 __major__ = 1
 __minor__ = 0
 __micro__ = 0
 
 
+NAME = 'decat'
+DESCRIPTION = """De-concatenate strings with that do not have white-spaces."""
+AUTHOR = 'Mandeep Singh'
 VERSION = f'{__major__}.{__minor__}.{__micro__}'
-BASE = Path(__file__).parent.parent
-PROJECT = join(BASE, 'decat')
-_VOCABULARY = join(PROJECT, '.data', 'vocabulary')
+BASE = Path(__file__).parent.parent.resolve()
+SOURCE = join(BASE, 'decat')
+_DATA = join(SOURCE, '.data')
+_VOCABULARY = join(_DATA, 'vocabulary')
 SUPPORTED_LANGUAGES = dict(english='en')
 VOCABULARY_MAP = dict(zip(SUPPORTED_LANGUAGES.values(),
                           map(lambda x: join(_VOCABULARY, x, 'tokens.json'),
