5835660cea815f34456d7dcd76988f6879d7dbf6
sudoMode <singh.mandeep2207@gmail.com>
2021-09-10 19:00:41 +0530

    added option to view version

--- decat/parsers/input_parser/_parser.py
+++ decat/parsers/input_parser/_parser.py
@@ -26,7 +26,7 @@ def parse_user_args(command_line=None):
                              'This switch accepts either a piece of text'
                              'or a valid file path to read text from.'
                         )
-    parser.add_argument('--version', '-V', default=None, action='store_true',
+    parser.add_argument('--version', '-v', default=None, action='store_true',
                         help='Display the version of the program')
     args = parser.parse_args(command_line)
     _validate_user_args(args, parser)
