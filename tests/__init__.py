3cb128aa89e7d48b5f7533babf5e19d995ee8ef2
sudoMode <singh.mandeep2207@gmail.com>
2026-09-04 13:44:18 +0530

    updated tests to better detect input files

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,21 +1,20 @@
+from pathlib import Path
 from decat import decat
 import json
+from unittest import TestCase, main
 
-from unittest import TestCase
-from unittest import main
-
-EXTRACTOR_INPUT = 'extractor_input.json'
-PASSAGE_INPUT = 'passage_input.json'
+BASE_DIR = Path(__file__).resolve().parent
+EXTRACTOR_INPUT = BASE_DIR / "extractor_input.json"
+PASSAGE_INPUT = BASE_DIR / "passage_input.json"
 
 
 class DecatTester(TestCase):
-
     @classmethod
     def setUpClass(cls):
-        with open(EXTRACTOR_INPUT, 'r') as f:
+        with open(EXTRACTOR_INPUT, "r") as f:
             cls.extractor_input = json.loads(f.read())
 
-        with open(PASSAGE_INPUT, 'r') as f:
+        with open(PASSAGE_INPUT, "r") as f:
             cls.passage_input = json.loads(f.read())
 
     @classmethod
@@ -35,18 +34,25 @@ class DecatTester(TestCase):
 
     def test_passage_decat(self):
         for passage in self.passage_input:
-            out = decat(passage['input'])
-            self.assertEqual(out, passage['output'])
+            out = decat(passage["input"])
+            self.assertEqual(out, passage["output"])
 
     def test_presevervation_of_special_characters(self):
         input_ = {
-                    'stringwithoutspace,andwithacomma':
-                    ['string', 'without', 'space,', 'and', 'with', 'a', 'comma']
+            "stringwithoutspace,andwithacomma": [
+                "string",
+                "without",
+                "space,",
+                "and",
+                "with",
+                "a",
+                "comma",
+            ]
         }
         for key, value in input_.items():
             out = decat(key, preserve_special_characters=True)
             self.assertEqual(out, value)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
